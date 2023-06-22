9945503641ed12865223153aceb515ec86504f58
Mariachi in a Jar <mariachiinajar@gmail.com>
2023-06-22 15:35:29 +0900

    Configured levels

---
+++ level1/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket         = "terraform-remote-state-for-pr"
+    key            = "levle1.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "remote-state-PR"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
